d1aebff318f864a2d1efb299cd852a16072f8b28
Daniel Jara P <djara@corebox.cl>
2018-09-15 20:04:40 -0300

    init

---
+++ App.vue
@@ -0,0 +1,33 @@
+<template>
+  <root>
+    <app-navigation></app-navigation>
+  </root>
+</template>
+
+<script>
+import Vue from "vue-native-core";
+import { VueNativeBase } from "native-base";
+import { StackNavigator, TabNavigator, TabBarBottom } from "vue-native-router";
+import { Root } from "native-base";
+
+import Home from "./src/home.vue";
+import Detail from "./src/detail.vue";
+
+// registering all native-base components to the global scope of the Vue
+Vue.use(VueNativeBase);
+
+const AppNavigation = StackNavigator(
+  {
+    Home: Home,
+    Detail: Detail
+  },
+  {
+    initialRouteName: "Home",
+    headerMode: "none"
+  }
+);
+
+export default {
+  components: { Root, AppNavigation }
+};
+</script>
---
+++ src/detail.vue
@@ -0,0 +1,21 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">HELLO</text>
+    <text class="text-color-primary">Vue Native App</text>
+    <text class="text-color-primary">on detail screen</text>
+    </view>
+</template>
+
+
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
